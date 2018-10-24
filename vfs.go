438822859819c3169b9bf74ece35a111b93ca121
Erik Gilling <konkers@google.com>
2018-10-23 23:10:49 -0700

    Add full path from GOROOT to vfs checks.

--- vfs.go
+++ vfs.go
@@ -3,6 +3,7 @@ package roll
 import (
 	"io"
 	"os"
+	"path"
 
 	"github.com/konkers/roll/data"
 )
@@ -15,6 +16,12 @@ func (b *Bot) openFile(filename string) (io.ReadCloser, error) {
 		return file, nil
 	}
 
+	// For dev purposes, check the source location
+	file, err = os.Open(path.Join("src/github.com/konkers/roll/data", filename))
+	if err == nil {
+		// Found it.
+		return file, nil
+	}
 	// If that didn't work, try in the archive.
 	return data.Assets.Open(filename)
 }
